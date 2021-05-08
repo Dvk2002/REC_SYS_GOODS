f1a993492f6fc04a227ace5b7824f8e0875cb78b
Dvk2002 <54672798+Dvk2002@users.noreply.github.com>
2021-05-08 19:26:15 +0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+
+
+def prefilter_items(data, take_n_popular=5000, item_features=None):
+    # Уберем не интересные для рекоммендаций категории (department)
+    if item_features is not None:
+        department_size = pd.DataFrame(item_features. \
+                                       groupby('department')['item_id'].nunique(). \
+                                       sort_values(ascending=False)).reset_index()
+
+        department_size.columns = ['department', 'n_items']
+        rare_departments = department_size[department_size['n_items'] < 150].department.tolist()
+        items_in_rare_departments = item_features[
+            item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+        data = data[~data['item_id'].isin(items_in_rare_departments)]
+
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
+    data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
+    data = data[data['price'] > 2]
+
+    # Уберем слишком дорогие товарыs
+    data = data[data['price'] < 50]
+
+    # Возбмем топ по популярности
+    popularity = data.groupby('item_id')['quantity'].sum().reset_index()
+    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
+
+    top = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist()
+
+    # Заведем фиктивный item_id (если юзер покупал товары из топ-5000, то он "купил" такой товар)
+    data.loc[~data['item_id'].isin(top), 'item_id'] = 999999
+
+
+    return data
+
+def postfilter_items(user_id, recommednations):
+    pass
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                #                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                #                     df[col] = df[col].astype(np.float16)
+                #                 elif
+
+                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    #         else:
+    #             df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
